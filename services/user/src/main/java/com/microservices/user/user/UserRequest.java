dff55f30e7781a4c31ebf651b228349cbc62546f
Doan Danh Du <22520260@gm.uit.edu.vn>
2025-05-16 20:46:06 +0700

    resolve conflict

--- services/user/src/main/java/com/microservices/user/user/UserRequest.java
+++ services/user/src/main/java/com/microservices/user/user/UserRequest.java
@@ -3,11 +3,17 @@ package com.microservices.user.user;
 public record UserRequest(
                 String id,
                 String keycloakId,
+                String username,
+                String password,
+                String email,
+                String firstName,
+                String lastName,
+                String role,
                 String biography,
                 String yearOfBirth,
                 String yearOfExperience,
                 String avatarUrl,
                 String backgroundUrl,
                 String phone,
-                String content
-                ) {}
+                String content) {
+}
