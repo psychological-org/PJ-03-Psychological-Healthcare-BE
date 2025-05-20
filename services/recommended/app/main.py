d6e05e6225aa120104c17a565fb20486839d80a3
ITechIOI <22520783@gm.uit.edu.vn>
2025-05-20 19:05:59 +0700

    feat(service): Open route to new recommended service

---
+++ services/recommended/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/v1/recommended/search-similar")
+def search_similar_food():
+    return "search-similar endpoint is working"
+
